9da7a010a81d48409116f096477c295cb20ccaf9
WSL - Roberto Olvera-Hernandez <rolvhdez@ccg.unam.mx>
2025-04-08 16:03:34 -0600

    changed: no filters needed as QC should be in another process (at least for genotype); DX output directory bug solved

--- 02_barebones-applet/run_job.sh
+++ 02_barebones-applet/run_job.sh
@@ -8,7 +8,6 @@ mkdir -p $OUTPUT_DIR
 
 ## Default inputs
 OUTPUT_PREFIX="$(date +'%Y%m%d_%H%M%S')"
-DXOUTPUT="/Users/Roberto/results/fgwas/${OUTPUT_PREFIX}/"
 UPLOAD="no"
 MAF=0.05
 MISSING=0.02
@@ -27,12 +26,15 @@ while [[ "$#" -gt 0 ]]; do
         --missing) MISSING="$2"; shift ;;
         --chr_range) CHR_RANGE="$2"; shift ;;
         --cpu) CPU="$2"; shift ;;
-        --dx-output) DXOUTPUT="$2"; shift ;;
         --upload) UPLOAD="$2"; shift ;;
         *) echo "Unknown parameter: $1"; exit 1 ;;
     esac
     shift
 done
+
+# Make the directory
+DXOUTPUT="/Users/Roberto/results/fgwas/${OUTPUT_PREFIX}/"
+dx mkdir -p "${DXOUTPUT}"
 
 # Whole output with prefix
 OUTPUT="${OUTPUT_DIR%/}/${OUTPUT_PREFIX}"
@@ -70,32 +72,15 @@ fi
 echo "Phenotype and pedigree files created."
 echo "Check ${OUTPUT_DIR}"
 
-# PLINK: QC genotype data (Ziyatdinov et al., 2023)
-QCBED="${OUTPUT}_geno"
-
-if compgen -G "${QCBED}.*" > /dev/null; then
-    echo "QC genotype files already exist. Skipping QC step..."
-else
-    echo "Performing QC on genotype file..."
-    {
-        plink --bfile "$BED" \
-            --set-hh-missing --set-me-missing \
-            --mendel-duos \
-            --autosome --mind 0.05 \
-            --geno 0.02 --mac 1 \
-            --hwe 1e-30 \
-            --make-bed --out "$QCBED"
-    } 2>&1 | tee -a "${OUTPUT}_qc_plink.log"
-fi
-
 # PLINK: Segment QC chromosomes
 SEGMENT_DIR="${OUTPUT_DIR%/}/segments"
 if ! [ -d "$SEGMENT_DIR" ] > /dev/null; then
     mkdir -p "$SEGMENT_DIR"
-    echo "Segmenting ${QCBED}..."
+    # echo "Segmenting ${QCBED}..."
+    echo "Segmenting ${BED}..."
     {
         for chrom in $(seq "${CHR_RANGE}"); do
-            plink --bfile "$QCBED" --chr "${chrom}" --make-bed --out "${SEGMENT_DIR}/${OUTPUT_PREFIX}_chr_${chrom}"
+            plink --bfile "$BED" --chr "${chrom}" --make-bed --out "${SEGMENT_DIR}/${OUTPUT_PREFIX}_chr_${chrom}"
         done 
     } 2>&1 | tee -a "${OUTPUT}_segment_plink.log"
 fi
@@ -105,8 +90,7 @@ BED_PATTERN="${SEGMENT_DIR}/${OUTPUT_PREFIX}_chr_@"
 echo "Running FGWAS..."
 {
     gwas.py "$PHEN" --bed "$BED_PATTERN" --pedigree "$PED" \
-        --chr_range "$CHR_RANGE" \
-        --cpus "$CPU" --min_maf "$MAF" --max_missing "$MISSING" \
+        --chr_range "$CHR_RANGE" --cpus "$CPU" \
         --no_hdf5_out --out "$OUTPUT"
 } 2>&1 | tee -a "${OUTPUT}_fgwas.log"
 
@@ -115,7 +99,7 @@ conda deactivate
 # Copy and upload results
 if [ "$UPLOAD" == "yes" ]; then
     echo "Uploading results to ${DXOUTPUT}..."
-    dx upload "${OUTPUT_DIR%/}/*.{csv,txt,log,gz}$" --brief --path "${DXOUTPUT}"
+    dx upload "${OUTPUT_DIR%/}/*.{csv,txt,log,gz}" --brief --path "${DXOUTPUT}"
 else
     echo "Results are available in ${OUTPUT_DIR}"
 fi
