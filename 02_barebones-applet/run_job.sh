552b2556dc4216536e319781263aa64b01c065db
CW - Roberto Olvera-Hernandez <rolvhdez@ccg.unam.mx>
2025-04-08 00:37:08 +0000

    changed: removed default variables for input files (CW in mind); chrom range to int type and typo fixed; added quotes to avoid bugs; added check to not run twice genoQC; changed range parameter in chrom segmenting loop; commented uploads

--- 02_barebones-applet/run_job.sh
+++ 02_barebones-applet/run_job.sh
@@ -2,31 +2,25 @@
 set -euo pipefail
 
 # Define inputs
-## Files to run the FGWAS
-KING="/mnt/project/Data/KING-IBD/king_ibdseg_4th.seg"
-BED="/mnt/project/Data/GSAv2-Chip/data/pVCF/MCPS_Freeze_150.GT_hg38.pVCF"
-BASE="/mnt/project/Data/Baseline/MCPS BASELINE.csv"
-QCBASE="/tmp/FILTER_BASELINE.csv"
-
-## User inputs
+## Default inputs
 OUTPUT_PREFIX="$(date +'%Y%m%d_%H%M%S')"
 DXOUTPUT="/Users/Roberto/results/${OUTPUT_PREFIX}_FGWAS/"
 MAF=0.05
 MISSING=0.02
-CHR_RANGE="22"
+CHR_RANGE=22
 CPU=4
 
 while [[ "$#" -gt 0 ]]; do
     case $1 in
         -p|--prefix) OUTPUT_PREFIX="$2"; shift ;;
         # File inputs
-        --pheno) BASE="$2"; shift ;;
-        --king) KING="$2"; shift ;;
+        --phenotype) BASE="$2"; shift ;;
+        --kinship) KING="$2"; shift ;;
         --genotype) BED="$2"; shift ;;
         # Filters
         --maf) MAF="$2"; shift ;;
         --missing) MISSING="$2"; shift ;;
-        --chr_range) CHRRANGE="$2"; shift ;;
+        --chr_range) CHR_RANGE="$2"; shift ;;
         --cpu) CPU="$2"; shift ;;
         --dx-output) DXOUTPUT="$2"; shift ;;
         *) echo "Unknown parameter: $1"; exit 1 ;;
@@ -51,6 +45,7 @@ fi
 conda activate "$CONDA_ENV";
 
 # Filter the Baseline Survey for BMI
+QCBASE="/tmp/FILTER_BASELINE.csv"
 if ! [ -f "$QCBASE" ]; then
     echo "Filtering baseline..."
     Rscript resources/filter_baseline.r "$BASE" "$QCBASE"
@@ -63,8 +58,8 @@ PED="/tmp/${OUTPUT_PREFIX}_pedigree.txt"
 if ! [ -f "$PHEN" ] || ! [ -f "$PED" ]; then
     {
         python resources/generate_inputs.py \
-            --kinship $KING --baseline $QCBASE \
-            --output-prefix $OUTPUT_PREFIX
+            --kinship "$KING" --baseline "$QCBASE" \
+            --output-prefix "$OUTPUT_PREFIX"
     } 2>&1 | tee -a "${OUTPUT_DIR}/${OUTPUT_PREFIX}_inputs.log"
 fi
 echo "Phenotype and pedigree files created."
@@ -73,22 +68,26 @@ echo "Check /tmp/"
 # PLINK: QC genotype data (Ziyatdinov et al., 2023)
 QCBED="/tmp/${OUTPUT_PREFIX}_genotype"
 
-echo "Performing QC on genotype file..."
-{
-    plink --bfile "$BED" \
-        --set-hh-missing --set-me-missing \
-        --mendel-duos \
-        --autosome --mind 0.05 \
-        --geno 0.02 --mac 1 \
-        --hwe 1e-30 \
-        --make-bed --out "$QCBED"
-} 2>&1 | tee -a "${OUTPUT_DIR}/${OUTPUT_PREFIX}_qc_plink.log"
+if compgen -G "${QCBED}.*" > /dev/null; then
+    echo "QC genotype files already exist. Skipping QC step..."
+else
+    echo "Performing QC on genotype file..."
+    {
+        plink --bfile "$BED" \
+            --set-hh-missing --set-me-missing \
+            --mendel-duos \
+            --autosome --mind 0.05 \
+            --geno 0.02 --mac 1 \
+            --hwe 1e-30 \
+            --make-bed --out "$QCBED"
+    } 2>&1 | tee -a "${OUTPUT_DIR}/${OUTPUT_PREFIX}_qc_plink.log"
+fi
 
 # PLINK: Segment QC chromosomes
 if ! compgen -G "/tmp/chr_22.*" > /dev/null; then
     echo "Segmenting ${QCBED}..."
     {
-        for chrom in {1..22}; do
+        for chrom in $(seq "${CHR_RANGE}"); do
             plink --bfile "$QCBED" --chr "${chrom}" --make-bed --out "/tmp/chr_${chrom}"
         done 
     } 2>&1 | tee -a "${OUTPUT_DIR}/${OUTPUT_PREFIX}_segment_plink.log"
@@ -109,6 +108,6 @@ echo "Running FGWAS..."
 conda deactivate
 
 # Copy and upload results
-cp $PHEN $PED $OUTPUT_DIR
-dx mkdir -p $DXOUTPUT
-dx upload --recursive $OUTPUT_DIR --brief --path $DXOUTPUT
+# cp "${PHEN}" "${PED}" "${OUTPUT_DIR}"
+# dx mkdir -p "${DXOUTPUT}"
+# dx upload --recursive "${OUTPUT_DIR}" --brief --path "${DXOUTPUT}"
